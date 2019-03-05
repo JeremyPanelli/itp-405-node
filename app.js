5f1da998c45fc484c028fdda7d658ae873bfc235
JEREMY-LAPTOP\Jeremy <panelli@usc.edu>
2019-03-05 13:19:34 -0800

    node lab

---
+++ app.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let knex = require('knex');
+
+let app = express();
+
+app.get('/api/artists', function(request, response){
+  let connection = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: 'chinook.db'
+    }
+  });
+  console.log(request.query);
+  if(request.query.filter != null){
+    connection
+      .select()
+      .from('artists')
+      .where('ArtistId', request.query.filter)
+      .then((artists) => {
+          if(artists){
+            response.json(artists);
+          } else {
+            response.status(404).json({
+              error: 'Artist not found'
+            });
+          }
+      });
+  }
+  else {
+    connection
+      .select()
+      .from('artists')
+      .then((artists) => {
+          if(artists){
+            response.json(artists);
+          } else {
+            response.status(404).json({
+              error: 'Artist not found'
+            });
+          }
+      });
+  }
+});
+
+app.listen(process.env.PORT || 8000);
