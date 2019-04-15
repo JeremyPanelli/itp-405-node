7762980d2f4f1b4996c81db5b6c22287c189cbf9
JEREMY-LAPTOP\Jeremy <panelli@usc.edu>
2019-04-15 12:10:06 -0700

    updated for websockets

--- app.js
+++ app.js
@@ -2,55 +2,55 @@ let express = require('express');
 let knex = require('knex');
 
 let app = express();
+//
+// app.get('/api/artists', function(request, response){
+//   let connection = knex({
+//     client: 'sqlite3',
+//     connection: {
+//       filename: 'chinook.db'
+//     }
+//   });
+//   console.log(request.query);
+//   if(request.query.filter != null){
+//     connection
+//       .select()
+//       .from('artists')
+//       .where('ArtistId', request.query.filter)
+//       .then((artists) => {
+//           if(artists){
+//             response.json(artists);
+//           } else {
+//             response.status(404).json({
+//               error: 'Artist not found'
+//             });
+//           }
+//       });
+//   }
+//   else {
+//     connection
+//       .select()
+//       .from('artists')
+//       .then((artists) => {
+//           if(artists){
+//             response.json(artists);
+//           } else {
+//             response.status(404).json({
+//               error: 'Artist not found'
+//             });
+//           }
+//       });
+//   }
+// });
+//
 
-app.get('/api/artists', function(request, response){
-  let connection = knex({
-    client: 'sqlite3',
-    connection: {
-      filename: 'chinook.db'
-    }
-  });
-  console.log(request.query);
-  if(request.query.filter != null){
-    connection
-      .select()
-      .from('artists')
-      .where('ArtistId', request.query.filter)
-      .then((artists) => {
-          if(artists){
-            response.json(artists);
-          } else {
-            response.status(404).json({
-              error: 'Artist not found'
-            });
-          }
-      });
-  }
-  else {
-    connection
-      .select()
-      .from('artists')
-      .then((artists) => {
-          if(artists){
-            response.json(artists);
-          } else {
-            response.status(404).json({
-              error: 'Artist not found'
-            });
-          }
-      });
-  }
+
+let WebSocket = require('ws');
+let wss = new WebSocket.Server({
+  port:process.env.PORT || 8080
 });
-
-app.listen(process.env.PORT || 8000);
-
-const WebSocket = require("ws");
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", ws => {
-  ws.on("message", message => {
-    wss.clients.forEach(client => {
+wss.on("connection", function(ws){
+  ws.on("message", function(message){
+    wss.clients.forEach((client) => {
       client.send(message);
     });
   });
